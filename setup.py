b6313e3a9302c0415dd06ed21aa8040f2cd870f3
Zhao Tang <zxt@zhaotang.ca>
2021-04-26 16:26:32 -0400

    declare Pillow dependency in setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/zxt/sv-cardgenerator",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'Pillow',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
